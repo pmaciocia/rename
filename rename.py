bb5e3ad4d17e7b6c14a719df03169f03a6b71fbd
Peter Maciocia <peter.maciocia@gmail.com>
2013-05-01 19:02:23 +0100

    taka file names

--- rename.py
+++ rename.py
@@ -7,6 +7,7 @@ def main():
     file1 = open("Naruto Shippuuden.txt").read()
     names = file1.split('\n')
 
+    # Naruto Shippuuden - 1x01 - Homecoming
     r=re.compile('(Naruto Shippuuden) - ([0-9]{1,2}x[0-9]{2}) - (.*)')
 
     count = 0
@@ -17,12 +18,14 @@ def main():
             count += 1
             season, episode = match.group(2).split('x')
             title = match.group(3)
-            details[count] = ( int(season), int(episode), title ) 
+            details[count] = ( int(season), int(episode), title )
 
     file2 = open("list.txt").read()
 
     f_names = file2.split('\n')
-    r=re.compile('(Naruto Shippuuden) - ([0-9]{3})-?([0-9]{3})?.(.*)')
+
+    # [Taka]_Naruto_Shippuuden_230_[720p][4EC3BBF6].mp4
+    r=re.compile('(Naruto[_\-.]Shippuuden)[^0-9]*([0-9]{3})-?([0-9]{3})?[^.]*.(.*)')
     file_names = []
     for f in f_names:
         match = r.search(f)
@@ -41,14 +44,14 @@ def main():
     for key, values in d.items():
         format = {}
 
-        ((season, episode, name), type) = values[0] 
+        ((season, episode, name), type) = values[0]
         season = str(season)
         season_pad = season.zfill(2)
         episode = str(episode).zfill(2)
 
         if len(values) == 2:
             ((_, episode2, title2),_) = values[1]
-            episode += "_" + str(episode2).zfill(2)
+            episode += "-" + str(episode2).zfill(2)
             name = name + " & " + title2
 
 
@@ -59,7 +62,7 @@ def main():
         format['name'] = name
         format['type'] = type
 
-        print ('mv "%(src)s" "Season %(season)s/Naruto Shippuuden - %(season_pad)sx%(episode)s - %(name)s.%(type)s"') % (format) 
+        print ('mv "%(src)s" "Season %(season)s/Naruto Shippuuden - %(season_pad)sx%(episode)s - %(name)s.%(type)s"') % (format)
 
 
 if __name__ == "__main__":
